537f9197c0956a27287599bd06f7f197cb967c56
Administrator <koloces@163.com>
2019-09-18 18:44:03 +0800

    初次提交

---
+++ app/src/main/java/com/koloce/app/App.java
@@ -0,0 +1,9 @@
+package com.koloce.app;
+
+import com.koloce.kulibrary.base.BaseApp;
+
+/**
+ * Created by koloces on 2019/9/18
+ */
+public class App extends BaseApp {
+}
---
+++ app/src/main/java/com/koloce/app/MainActivity.java
@@ -0,0 +1,36 @@
+package com.koloce.app;
+
+import com.koloce.kulibrary.base.UIActivity;
+
+public class MainActivity extends UIActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void afterInitView() {
+
+    }
+
+    @Override
+    protected void initBeforeSetContentView() {
+
+    }
+
+    @Override
+    protected void initBeforeInitView() {
+
+    }
+}
