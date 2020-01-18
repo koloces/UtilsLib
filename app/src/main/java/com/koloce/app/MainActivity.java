671d795dceb6052ef0c51af90dcce328d0e387ad
Administrator <koloces@163.com>
2020-01-18 10:00:57 +0800

    初次提交

--- app/src/main/java/com/koloce/app/MainActivity.java
+++ app/src/main/java/com/koloce/app/MainActivity.java
@@ -1,6 +1,12 @@
 package com.koloce.app;
 
+import android.view.View;
+
 import com.koloce.kulibrary.base.UIActivity;
+import com.koloce.kulibrary.listener.OnOpenAlbumResultListener;
+import com.luck.picture.lib.entity.LocalMedia;
+
+import java.util.List;
 
 
 public class MainActivity extends UIActivity {
@@ -16,7 +22,17 @@ public class MainActivity extends UIActivity {
 
     @Override
     protected void initListener() {
+        findViewById(R.id.Btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAlbum(1, null, 101, new OnOpenAlbumResultListener() {
+                    @Override
+                    public void onResult(int requestCode, List<LocalMedia> result) {
 
+                    }
+                });
+            }
+        });
     }
 
     @Override
